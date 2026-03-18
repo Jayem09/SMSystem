2abaee327ca6590f3a09f02f755baeb3a0bec8f7
Jayem09 <johndinglasan12@gmail.com>
2026-03-18 14:15:15 +0800

    Strengthen CORS and add detailed debug error logs v0.2.7

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -55,7 +55,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	
+	// CORS Middleware
 	router.Use(func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
@@ -63,9 +63,10 @@ func main() {
 		} else {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
